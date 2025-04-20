5e993a454f85bfc2f016ea94fcc519dc20e5212a
Martin Liu <97211704+martin01021207@users.noreply.github.com>
2025-04-20 17:24:26 -0500

    Update MakeVariables.py

--- utilities/MakeVariables.py
+++ utilities/MakeVariables.py
@@ -2,33 +2,24 @@ import numpy as np
 
 nChannels = 24
 inIceChannels = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 21, 22, 23]
+nChannels_inIce = len(inIceChannels)
 
 
 def getReferenceTraceIndices(vars, find_max=False):
-    refIndex_PA = 0
-    refIndex_inIce = 0
-    refIndex_surface = 0
+    if len(vars) not in {nChannels, nChannels_inIce}:
+        raise ValueError("Need vars for all 24 channels or for all 15 in-ice channels.")
 
-    if len(vars) != nChannels:
-        raise ValueError("Need vars for all 24 channels...")
-    else:
-        vars_PA = np.array( vars[:4] )
-        vars_inIce = np.array([])
-        vars_surface = np.array([])
+    inIceOnly = len(vars) == nChannels_inIce
+    vars = np.array(vars)
 
-        for i in range(nChannels):
-            if i in inIceChannels:
-                vars_inIce = np.append(vars_inIce, vars[i])
-            else:
-                vars_surface = np.append(vars_surface, vars[i])
+    vars_PA = vars[:4]
+    vars_inIce = vars if inIceOnly else vars[inIceChannels]
+    vars_surface = [] if inIceOnly else np.delete(vars, inIceChannels)
 
-        if find_max:
-            refIndex_PA = vars_PA.argmax()
-            refIndex_inIce = vars_inIce.argmax()
-            refIndex_surface = vars_surface.argmax()
-        else:
-            refIndex_PA = vars_PA.argmin()
-            refIndex_inIce = vars_inIce.argmin()
-            refIndex_surface = vars_surface.argmin()
+    func = np.argmax if find_max else np.argmin
+
+    refIndex_PA = func(vars_PA)
+    refIndex_inIce = func(vars_inIce)
+    refIndex_surface = func(vars_surface) if not inIceOnly else 0
 
     return refIndex_PA, refIndex_inIce, refIndex_surface
