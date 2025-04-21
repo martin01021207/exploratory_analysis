625038b3bbafacb980649267fcacaded963802fc
Martin Liu <97211704+martin01021207@users.noreply.github.com>
2025-04-21 16:17:05 -0500

    Update PyTorch_Generate_CNN_Model.py

--- PyTorch_Generate_CNN_Model.py
+++ PyTorch_Generate_CNN_Model.py
@@ -13,16 +13,13 @@ class Reshape(torch.nn.Module):
 net = torch.nn.Sequential(
     Reshape(),
     nn.Conv2d(1, 10, kernel_size=3, padding=1),
-    nn.ReLU(),
     nn.BatchNorm2d(10),
-    nn.Conv2d(10, 10, kernel_size=3, padding=1),
     nn.ReLU(),
-    nn.MaxPool2d(kernel_size=2),
     nn.Conv2d(10, 10, kernel_size=3, padding=1),
     nn.ReLU(),
     nn.MaxPool2d(kernel_size=2),
     nn.Flatten(),
-    nn.Linear(10*8*8, 256),
+    nn.Linear(10*16*16, 256),
     nn.ReLU(),
     nn.Linear(256, 2),
     nn.Sigmoid()
