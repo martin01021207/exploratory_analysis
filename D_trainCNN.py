e4b973cb9b0c223047eebfb6760f586cc57529b7
Martin Liu <97211704+martin01021207@users.noreply.github.com>
2025-06-04 23:32:07 +0800

    Update D_trainCNN.py

--- D_trainCNN.py
+++ D_trainCNN.py
@@ -74,22 +74,22 @@ dataLoader = TMVA.DataLoader(f"dataLoader_images_s{station_number}")
 
 imgSize = N * N
 
-inputFileName_sig_train = f"images_s{station_number}_sig_train.root"
+inputFileName_sig_train = f"train/images_s{station_number}_sig_train.root"
 inputFile_sig_train = TFile.Open(dir_in+inputFileName_sig_train)
 if inputFile_sig_train is None:
     ROOT.Warning("TMVA_CNN_Classification", "Error opening input file %s (SIGNAL TRAINING) - exit", inputFileName_sig_train.Data())
 
-inputFileName_sig_test = f"images_s{station_number}_sig_test.root"
+inputFileName_sig_test = f"test/images_s{station_number}_sig_test.root"
 inputFile_sig_test = TFile.Open(dir_in+inputFileName_sig_test)
 if inputFile_sig_test is None:
     ROOT.Warning("TMVA_CNN_Classification", "Error opening input file %s (SIGNAL TESTING) - exit", inputFileName_sig_test.Data())
 
-inputFileName_bkg_train = f"images_s{station_number}_bkg_train.root"
+inputFileName_bkg_train = f"train/images_s{station_number}_bkg_train.root"
 inputFile_bkg_train = TFile.Open(dir_in+inputFileName_bkg_train)
 if inputFile_bkg_train is None:
     ROOT.Warning("TMVA_CNN_Classification", "Error opening input file %s (BACKGROUND TRAINING) - exit", inputFileName_bkg_train.Data())
 
-inputFileName_bkg_test = f"images_s{station_number}_bkg_test.root"
+inputFileName_bkg_test = f"test/images_s{station_number}_bkg_test.root"
 inputFile_bkg_test = TFile.Open(dir_in+inputFileName_bkg_test)
 if inputFile_bkg_test is None:
     ROOT.Warning("TMVA_CNN_Classification", "Error opening input file %s (BACKGROUND TESTING) - exit", inputFileName_bkg_test.Data())
